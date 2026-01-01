f5b397d064dff6c3f660abab85e1c0239bbc7cd8
MalaRidwan <malariidwan@gmail.com>
2026-01-02 05:07:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ const qrcode = require('qrcode-terminal');
 const axios = require('axios');
 
 // --- PENGATURAN BOT ---
-const NOMOR_BOT = '6285156906427'; // Nomor Bot lu yang baru, Wan!
+const NOMOR_BOT = '6285156906427'; 
 const API_KEY_GEMINI = 'AIzaSyBzxNr68JjBPG4wNC9NK-hl9opRDQ1kBaw'; 
 const URL_GEMINI = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+        // JALAN NINJA BUAT TERMUX:
+        executablePath: '/data/data/com.termux/files/usr/bin/chromium', 
         handleSIGINT: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process', // Biar hemat RAM di HP lu
+            '--disable-gpu'
+        ]
     }
 });
 
@@ -21,11 +32,13 @@ client.on('qr', (qr) => {
 });
 
 client.on('ready', () => {
-    console.log('Bot Yui V.68 udah Online, Wan! Siap layanin tester lu di 0895320302320.');
+    console.log('----------------------------');
+    console.log('Yui V.69 UDAH ONLINE, WAN!');
+    console.log('Siap tempur di Termux Poco X6.');
+    console.log('----------------------------');
 });
 
 client.on('message', async (msg) => {
-    // Bot cuma bales chat, lu bisa tes dari nomor tester lu tadi
     if (msg.body) {
         try {
             const response = await axios.post(`${URL_GEMINI}?key=${API_KEY_GEMINI}`, {
@@ -41,7 +54,7 @@ client.on('message', async (msg) => {
 
 async function startBot() {
     try {
-        console.log('SEDANG MENGAMBIL KODE PAIRING UNTUK ' + NOMOR_BOT + '...');
+        console.log('SEDANG MENGAMBIL KODE PAIRING...');
         await client.initialize();
         const pairingCode = await client.requestPairingCode(NOMOR_BOT);
         console.log('----------------------------');
